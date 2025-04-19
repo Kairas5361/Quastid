31f6f1978aeb3351a311023e7b25ead3e2d21bef
Kairas <150584690+Kairas5361@users.noreply.github.com>
2025-04-19 22:06:55 +0300

    Create firebase.js

---
+++ public/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p7",
+  authDomain: "reddit-klon.firebaseapp.com",
+  projectId: "reddit-klon",
+  storageBucket: "reddit-klon.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:abcdef1234567890"
+};
+
+// Firebase'i başlat
+firebase.initializeApp(firebaseConfig);
